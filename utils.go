332286d623ca2aefd2d3f174fe31781589ae3feb
Jon Morehouse <morehousej09@gmail.com>
2015-10-18 20:43:56 -0700

    datebook: first version + structuring

--- utils.go
+++ utils.go
@@ -14,4 +14,25 @@ func DateDirectory(date time.Time) string {
 	return path.Join(fmt.Sprintf("%d", year), name)
 }
 
+func StripNewLines(buf []byte) []byte {
+	var startIndex, rightOffset int
 
+	for index, word := range buf {
+		if word == byte('\n') {
+			continue
+		}
+
+		startIndex = index
+		break
+	}
+
+	for index := len(buf)- 1; index > startIndex; index-- {
+		if buf[index] != byte('\n') {
+			break
+		}
+
+		rightOffset++
+	}
+
+	return buf[startIndex:len(buf) - rightOffset]
+}
